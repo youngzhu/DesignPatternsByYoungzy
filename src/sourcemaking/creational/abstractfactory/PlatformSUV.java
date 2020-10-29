c5154c1865bd2f62b0b779eb71b8f76389fc59f4
Young-Win <young@win.com>
2020-10-29 11:43:34 +0800

    抽象工厂：结构调整，从“产品封装”到“平台封装”

--- src/sourcemaking/creational/abstractfactory/PlatformSUV.java
+++ src/sourcemaking/creational/abstractfactory/PlatformSUV.java
@@ -1,17 +1,19 @@
 package sourcemaking.creational.abstractfactory;
 
-class SedanHood extends Hood {
+class SUVDoor extends Door {
     void sayHi() {
-        System.out.println("Hi, I'm a Sedan Hood");
+        System.out.println("Hi, I'm a SUV Door");
     }
 }
-class SportsHood extends Hood {
-    void sayHi() {
-        System.out.println("Hi, I'm a Sports Hood");
-    }
-}
+
 class SUVHood extends Hood {
     void sayHi() {
         System.out.println("Hi, I'm a SUV Hood");
+    }
+}
+
+class SUVWheel extends Wheel {
+    void sayHi() {
+        System.out.println("Hi, I'm a SUV Wheel");
     }
 }
